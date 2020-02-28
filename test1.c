c03445f477b4ccc0159501e4f12482b6292670c8
Chauncey1991 <373375525@qq.com>
2020-02-28 19:31:01 +0800

    新加文件

--- test1.c
+++ test1.c
@@ -2,107 +2,24 @@
 #include "string.h"
 #include "stdlib.h"
 
-int is_include_same_char(char * str) {
-	if (str == NULL) {
-		return -1;
-	}
+int lengthOfLongestSubstring(char * s){
+    int i;
+    int ret = 0;
+    int tmp = 0;
+    int table[128] = {0}; //char
 
-	int length;
-	length = strlen(str);
-
-	if (length == 0) {
-		return 0;
-	}
-
-	int i , j;
-	for(i =0; i < length; i++) {
-		for(j = i + 1; j < length; j++) {
-			if (str[j] == str[i]) {
-				return 1;
-			}
-		}
-	}
-
-	return 0;
-}
-
-int lengthOfLongestSubstring(char * s) {
-	if (s == NULL) {
-		return 0;
-	}
-
-	int length;
-	length = strlen(s);
-	if(length == 0) {
-		return 0;
-	}
-
-	int length1;
-	length1 = length;
-	char * s1 = NULL;
-
-	s1 = (char *)malloc(length + 1);
-	if(s1 == NULL) {
-		return 0;
-	}
-
-	int flag = 0;
-
-	while (length1 > 1) {
-	
-		int i;
-		for(i = 0; i <= (length - length1); i++) {
-			(void)memset(s1, 0, length + 1);
-			(void)strncpy(s1, &(s[i]), length1);
-
-			flag = is_include_same_char(s1);
-			if(flag == 0) {
-				return length1;
-			}
-		}
-
-		length1--;
-	}
-
-	return length1;
-}
-
-
-int lengthOfLongestSubstring1(char * s){
-	if (s == NULL) {
-		printf("input error.\r\n");
-		return -1;
-	}
-
-	int i, j, k;
-	int length, result = 0, result1 = 0, result2 = 0;
-	int flag = 0;
-	length = strlen(s);
-
-	for(i = 0; i < length; i++) {
-		for(j = i; j < length; j++) {
-			for (k = j + 1; k < length; k++) {
-				if(s[k] == s[j]) {
-					flag = 1;
-					result = k - j;	
-					if (result > result1) {
-						result1 =result;
-					}
-					break;							
-				}
-			}
-		}
-
-		if(result1 > result2) {
-			result2 = result1;
-		}
-	}
-
-	if(flag == 0) {
-		result2 = length;
-	}
-	
-	return result2;
+    for(i = 0; i < strlen(s); i++){
+        int index = (int)s[i];
+        if(table[index] == 0){
+            ++tmp;
+        } else {
+            ret = (ret >= tmp) ? ret : tmp;
+            tmp = (tmp >= i + 1 - table[index]) ? i + 1 - table[index] : ++tmp;
+        }
+        table[index] = i + 1;
+    }
+    ret = (ret >= tmp) ? ret : tmp;
+    return ret;
 }
 
 int main()
