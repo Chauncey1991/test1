d1f9bb0c32459be2cdc719d936c0b74fb27fe816
Chauncey1991 <373375525@qq.com>
2020-02-29 17:24:48 +0800

    使用滑动窗口算法实现

--- test1.c
+++ test1.c
@@ -2,7 +2,7 @@
 #include "string.h"
 #include "stdlib.h"
 
-int lengthOfLongestSubstring(char * s){
+/* int lengthOfLongestSubstring(char * s){
     int i;
     int ret = 0;
     int tmp = 0;
@@ -20,6 +20,51 @@ int lengthOfLongestSubstring(char * s){
     }
     ret = (ret >= tmp) ? ret : tmp;
     return ret;
+} */
+
+
+int lengthOfLongestSubstring(char * s) {
+    if(s == NULL) {
+        return 0;
+    }
+
+    int i, j, k;
+    int length = strlen(s);
+    int length1 = 0, length2 = 0;
+    int same_flag;
+
+    if (length <= 1) {
+        return length;
+    }
+
+    for(i = 0, j = 1; (i < length) && (j < length); i++) {
+        for(j = 1; j < length; ) {
+            same_flag = 0;
+            // 判断第j个字母，是否与i到j-1个字符重复
+            for (k = i; k < j; k++)
+            {
+                if(s[j] == s[k]) {
+                    same_flag = 1;
+                    break;
+                }
+            }
+
+            //如果重复，使j回退一格，跳出循环，开始移动i
+            if(same_flag == 1) {                
+                j--;
+                break;
+            } else {
+                length1 = k - i + 1;
+                j++;
+            } 
+        }
+
+        if(length1 > length2) {
+            length2 = length1;
+        }
+    }
+
+    return length2;
 }
 
 int main()
@@ -34,8 +79,6 @@ int main()
 	
 	int result = 0;
 	result = lengthOfLongestSubstring(s);
-
-//	result = is_include_same_char(s);
 
 	printf("The result is %d.\r\n", result);
 
